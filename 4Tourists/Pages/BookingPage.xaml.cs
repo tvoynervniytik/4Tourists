7c0cc83b6228589dae062331e8d6de89b81bca77
_EmPTi_ SFX <ramilgar008@gmail.com>
2023-11-29 14:22:24 +0300

    Кнопка добавления бронирования (Ильмир)

--- 4Tourists/Pages/BookingPage.xaml.cs
+++ 4Tourists/Pages/BookingPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _4Tourists.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,67 @@ namespace _4Tourists.Pages
     /// </summary>
     public partial class BookingPage : Page
     {
+        public static List<Booking> booking = new List<Booking>();
+        public static Booking bookingDel = new Booking();
+        public static Booking bookingEdit = new Booking();
         public BookingPage()
         {
             InitializeComponent();
+            booking = new List<Booking>(DBConnection.TouristsGo.Booking.ToList());
+            BookingSlv.ItemsSource = booking;
         }
-    }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddBookingWindow addBookingWindow = new AddBookingWindow();
+            addBookingWindow.Show();
+        }
+
+        public static void DeleteBooking(Booking booking)
+        {
+
+            DBConnection.TouristsGo.Booking.Remove(booking);
+            DBConnection.TouristsGo.SaveChanges();
+
+
+        }
+        public static List<Booking> SourceStudentsList(Booking employee)
+        {
+            return new List<Booking>(DBConnection.TouristsGo.Booking.ToList());
+        }
+        private void delBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (BookingSlv.SelectedItem != null)
+            {
+                DeleteBooking(BookingSlv.SelectedItem as Booking);
+                BookingSlv.SelectedItem = null;
+                BookingSlv.ItemsSource = SourceStudentsList(bookingDel);
+            }
+            else
+            {
+                MessageBox.Show("Выберите работника для удаления");
+            }
+        }
+
+
+        private void RefreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            booking = new List<Booking>(DBConnection.TouristsGo.Booking.ToList());
+            BookingSlv.ItemsSource = booking;
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (BookingSlv.SelectedItem != null)
+            {
+                bookingEdit = BookingSlv.SelectedItem as Booking;
+                EditBookingWindow editBookingWindow = new EditBookingWindow(bookingEdit);
+                editBookingWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Выберите работника для редактирования");
+            }
+        }
 }
